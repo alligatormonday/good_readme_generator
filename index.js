eafb7eef72f8b05b85e16c23a9c2eed1a7db1f57
alligatormonday <joey@joeyjepson.com>
2020-10-07 20:13:54 -0700

    Coded constant variables.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-// array of questions for user
-const questions = [
+// Constants
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
+
+// array of questions for user
+const questions = [{
+    /* Pass your questions in here */
+    type: 'input',
+    name: 'yourName',
+    message: 'What is your name?',
+},
+{
+    type: 'input',
+    name: 'languagesKnown',
+    message: 'What languages do you know?',
+},
+{
+    type: 'input',
+    name: 'preferredCommunication',
+    message: 'What is your preferred method of communication?',
+}
 ];
 
 // function to write README file
@@ -9,7 +31,9 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+// inquirer.prompt(
+    // pass questions
+)
 }
 
 // function call to initialize program
